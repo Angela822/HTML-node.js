029943f05c60ffbfb4629654e161dfeb11d2c470
Unknown <angelayu822@gmail.com>
2018-08-30 14:58:15 +0800

    update login.js

--- routes/login.js
+++ routes/login.js
@@ -16,24 +16,30 @@ router.post('/', function(req, res, next) {
     var nickName=req.param("nickName");
 
     pool.query('select * from users where userid=? and password=?', [userid, password], function(err, rows, fields) {
+        console.log(userid);
+        console.log(password);
+
         if (err){
 		    //如果失敗, 清除session中的資訊.
             req.session.loginPass=false;
-			req.session.userNo=''; 
+			
             req.session.userid=''; 
             res.render('loginFail', {});
 		}else if(rows.length==0){
 		    //如果帳/密不符, 清除session中的資訊.		
             req.session.loginPass=false;
-			req.session.userNo=''; 
+			
             req.session.userid=''; 		
             res.render('loginFail', {});
         }else{	
 		    //如果成功, 將登入者姓名記錄在session中.
             req.session.loginPass=true;
-            req.session.userNo=rows[0].userNo; 
+            
             req.session.userid=rows[0].userid; 
-            req.session.nickName=rows[0].nickName; 		
+            req.session.nickName=rows[0].nickName; 
+            req.session.sign=rows[0].sign; 
+            req.session.avatar=rows[0].avatar; 
+
             res.redirect('/loginSuccess');
 		}
     });
